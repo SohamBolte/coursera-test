298d97bca5c9c56945d7421eb26c3b8f3e375874
SohamBolte <96581825+SohamBolte@users.noreply.github.com>
2023-06-27 21:38:23 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,1 +1,14 @@
+(function () {
+  var speakWord = "Good Bye";
+
+  function speak(name) {
+    console.log(speakWord + " " + name);
+  }
+
+  var byeSpeaker = {
+    speak: speak
+  };
+
+  window.byeSpeaker = byeSpeaker;
+})();
 
